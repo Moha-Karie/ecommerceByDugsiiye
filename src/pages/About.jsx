3d2b8612a065ed55a5f3008376de7a973b45d919
Moha-Karie <karielapt@gmail.com>
2025-04-01 11:48:16 +0300

    Added Contacts

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,14 @@
 const About = () => {
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, voluptatum,
-      pariatur aspernatur est iste fugit fuga mollitia necessitatibus eos
-      maiores ex ut soluta cum aperiam, accusamus perspiciatis. Facere
-      aspernatur eaque vel, cumque nam praesentium laudantium modi dolorum enim
-      eius vitae assumenda possimus rerum excepturi, quam hic! Tempore laborum
-      veritatis repellendus voluptas quos nostrum officiis optio modi adipisci
-      dicta unde, molestias velit eveniet laudantium voluptatibus, dolor commodi
-      amet possimus quas recusandae? Magni debitis iure exercitationem ab
-      placeat! Doloremque itaque maxime vero, numquam quasi consectetur aperiam
-      repellat possimus quisquam sequi earum optio, quos rem quae debitis
-      necessitatibus cum tempore ipsa laboriosam fuga!
+      <fieldset>
+        <legend>Our Contacts</legend>
+        <ul>
+          <li>+252612232002</li>
+          <li>+252612232002</li>
+          <li>+252612232002</li>
+        </ul>
+      </fieldset>
     </div>
   );
 };
